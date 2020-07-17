2ae21964b2f948beb7067311cac4ff48646383ae
vivek1125 <v.raj91220@gmail.com>
2020-07-17 21:27:36 +0530

    Added all file

---
+++ Build/build.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "build.xml"
+pause
---
+++ CsharpCode/CsharpCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CsharpCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ms build comleted");
+            Console.Read();
+        }
+    }
+}
