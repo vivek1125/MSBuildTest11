7a795235d710dc1806d7d51352af4ed5c9089c81
vivek1125 <v.raj91220@gmail.com>
2020-07-20 21:40:44 +0530

    try wid bat

--- Build/build.bat
+++ Build/build.bat
@@ -1,2 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "build.xml"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "C:\Users\vivek.d.raj\source\MSBuildTest11\Build\build.xml"
 pause
+cd "C:\Users\vivek.d.raj\source\MSBuildTest11\CsharpCode\"
--- CsharpCode/Run.bat
+++ CsharpCode/Run.bat
@@ -1,2 +1,8 @@
-ssh-keygen && "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" remote set-url origin git@github.com:vivek1125/MSBuildTest11.git && "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" push -u origin master
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" remote -v
+pause
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" status
+pause
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" commit -a -m "try wid bat"
+pause
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" remote set-url origin https://github.com/vivek1125/MSBuildTest11.git && "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\git.exe" push -u origin master
 pause
