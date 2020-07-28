3ab90d225a3b14b3d742f9d90cd80827d0a7e3fb
vivek1125 <v.raj91220@gmail.com>
2020-07-28 15:16:52 +0530

    aa

--- Build/build.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "C:\Users\vivek.d.raj\source\MSBuildTest11\Build\build.xml"
-pause
-cd "C:\Users\vivek.d.raj\source\MSBuildTest11\CsharpCode\"
